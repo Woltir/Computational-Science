171efed08a22a2b5b9b8fcfcdad3de75335cd41a
Woltir <wolfus.ika@gmail.com>
2017-10-15 17:15:27 +0200

    Update mat.py
    Fix errors in my function...

--- _homework3/_exercise3/mat.py
+++ _homework3/_exercise3/mat.py
@@ -4,34 +4,42 @@ import matplotlib.pyplot as plt
 
 seed(1234)
 
-def in_square(x, y):
+def in_circle(x, y):
     # function that returns true if a point (x, y) is inside the unit circle,
     # and false otherwise
     if x*x + y*y <= 1:
         return True
     else : return False
 
+def in_square(x, y):
+    if (abs(x) <= 1. and abs(y) <= 1.) :
+        return True
+    else: return False
+
 def trajectory(nstep, step) :
-    traj    = np.zeros((nstep, 2)) # define trajectory set
-    step_x, step_y  = step, step          # define step size for x, y
-    acceptance = 0 #initialize acceptance
+    traj            = np.zeros((nstep, 2))  # define trajectory set
+    step_x, step_y  = step, step            # define step size for x, y
+    acceptance      = 0                     # initialize acceptance
+    circlecompt     = 0                     # initialiaze the circle counter to 0
     for i in range(nstep-1) :
         # propose a move between :
         # x +/- dx, with dx a r.v. in [-step, step]
+        x0, y0  = traj[i, 0], traj[i, 1]
         dx, dy  = (2*step_x*random()) - step_x, (2*step_y*random()) - step_y
-        x, y    = traj[i, 0] + dx , traj[i, 1] + dy     # move
+        x, y    = x0 + dx , y0 + dy     # move
         if in_square(x, y) :
-            # if the move is in the unit circle, update the trajectory, increase
+            # if the move is in the unit square, update the trajectory, increase
             # accceptance by one
             traj[i+1] = x, y
             acceptance += 1
+            if in_circle : circlecompt += 1
         else :
-            # else, stays here
-            traj[i+1] = traj[i]
-    return traj, acceptance/float(nstep)
+            # else, stays here 
+            traj[i+1] = x0, y0
+    return traj, acceptance/float(nstep), circlecompt
 
-#Question 1 :
-step = 1.1
+### Question 1 :
+step = 1.
 N = [100, 1000, 4000, 20000]
 T = [trajectory(N[i], step) for i in range(len(N))]
 
@@ -47,13 +55,15 @@ ax[1, 1].plot(T[3][0][:,0], T[3][0][:,1])
 ax[1, 1].set_title('n = ' + str(N[3]))
 plt.show()
 
-#Question 2 :
-S = np.linspace(0.01, 2., num = 100) # list of steps between 0.01 and 2.
-nstep = 10**3
+### Question 2 :
+S = np.linspace(0.1, 2., num = 100) # list of steps between 0.01 and 5.
+nstep = 10**4
 T = [trajectory(nstep, S[i]) for i in range(len(S))]
 
 A = np.array([T[i][1] for i in range(len(S))]) # acceptance list
 
 # print the best(s) empricial 1/2 step 
-print 'The best empirical 1/2 step size is step = ' + str( S[np.argwhere(abs(A - 0.5) <= 0.01 )])
+print 'The best empirical 1/2 step size is step = ' + str( S[np.argmin(abs(A - 0.5))])
 plt.plot(S, A) ; plt.title('Acceptance ratio vs space step') ; plt.show()
+
+### Question 3 :
